681231d7e6736e6ea00e3506aff2428f6d31339c
unknown <sandu.ionut@0315EITR0050.emag.local>
2015-06-13 17:33:42 +0300

    added logic for getQuestAction + getQuestListAction

--- src/eQuest/Api/QuestBundle/Controller/QuestController.php
+++ src/eQuest/Api/QuestBundle/Controller/QuestController.php
@@ -2,18 +2,30 @@
 
 namespace eQuest\Api\QuestBundle\Controller;
 
+use eQuest\Api\QuestBundle\Entity\Quest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class QuestController extends Controller
 {
     public function getQuestAction($id)
     {
-
+        $repository = $this->getDoctrine()->getRepository('eQuestApiQuestBundle:Quest');
+        $quest = $repository->findOneById($id);
+        return new JsonResponse($quest);
     }
 
     public function getQuestListAction()
     {
+        $repository = $this->getDoctrine()->getRepository('eQuestApiQuestBundle:Quest');
+        $quests = $repository->findAll();
         //toate questurile
+        return JsonResponse($quests);
     }
 
     public function setActiveQuestAction($user_id,$quest_id){
